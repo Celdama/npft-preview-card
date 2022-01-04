660be8d2a3527d5645977169a2151754a47638cb
Celdama <celdama.dev@gmail.com>
2022-01-04 17:47:49 +0100

    components: create Divider component

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,15 +1,17 @@
-import React from "react";
-import MainImage from "../MainImage";
-import NftInfo from "../NftInfo";
-import Divider from "../Divider";
-import { Wrapper, Content } from "./Card.styles";
+import React from 'react';
+import MainImage from '../MainImage';
+import NftInfo from '../NftInfo';
+import Divider from '../Divider';
+import { Wrapper, Content } from './Card.styles';
 
 const Card = () => (
   <Wrapper>
     <Content>
       <MainImage />
-      <NftInfo title="Equilibrium #3429" price="0.041" days={3} />
-      <Divider />
+      <div>
+        <NftInfo title='Equilibrium #3429' price='0.041' days={3} />
+        <Divider />
+      </div>
     </Content>
   </Wrapper>
 );
--- src/components/Divider/Divider.styles.js
+++ src/components/Divider/Divider.styles.js
@@ -1,6 +1,5 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
-export const Divider = styled.div`
-  border-bottom: 0.2px solid white;
-  background-color: var(--white);
+export const Divider = styled.hr`
+  border-color: var(--softBlue);
 `;
